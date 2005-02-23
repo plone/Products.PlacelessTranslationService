c30bb0a266b1c445b869f200360d17da865c9f46
Godefroid Chapelle <gotcha@bubblenet.be>
2005-02-23 11:24:30 +0000

    end of cleanup
    svn path=/branches/z3i18n-branch/; revision=3980

--- Products/PlacelessTranslationService/tests/test_z3messages.py
+++ Products/PlacelessTranslationService/tests/test_z3messages.py
@@ -1,13 +1,16 @@
 import sys
 
 import unittest
+import re 
 
 from StringIO import StringIO
 
+from TALDefs import NAME_RE
 from TAL.TALDefs import I18NError
 from TAL.tests.test_talinterpreter import TestCaseBase
 from TAL.TALInterpreter import TALInterpreter
 from TAL.DummyEngine import DummyEngine, Default
+from TAL.DummyEngine import DummyTranslationService, DummyDomain
 from DocumentTemplate.DT_Util import ustr
 
 from Products.PlacelessTranslationService.z3i18n import patch_TALInterpreter
@@ -15,11 +18,44 @@ from Products.PlacelessTranslationService.z3i18n.messageid import MessageID
 
 class Z3DummyEngine(DummyEngine):
     
+    def __init__(self, macros=None):
+        DummyEngine.__init__(self, macros)
+        self.translationService = Z3DummyTranslationService()
+    
     def evaluateText(self, expr):
         text = self.evaluate(expr)
         if text is not None and text is not Default and not isinstance(text, MessageID):
             text = ustr(text)
         return text
+
+class LowerDummyDomain:
+
+    def translate(self, msgid, mapping=None, context=None,
+                  target_language=None, default=None):
+        # This is a fake translation service which simply uppercases non
+        # ${name} placeholder text in the message id.
+        #
+        # First, transform a string with ${name} placeholders into a list of
+        # substrings.  Then upcase everything but the placeholders, then glue
+        # things back together.
+
+        # simulate an unknown msgid by returning None
+        if msgid == "don't translate me":
+            text = default
+        else:
+            text = msgid.lower()
+
+        def repl(m, mapping=mapping):
+            return ustr(mapping[m.group(m.lastindex).lower()])
+        cre = re.compile(r'\$(?:(%s)|\{(%s)\})' % (NAME_RE, NAME_RE))
+        return cre.sub(repl, text)
+    
+class Z3DummyTranslationService(DummyTranslationService):
+    def getDomain(self, domain):
+        if domain == 'lower':
+            return LowerDummyDomain()
+        else:
+            return DummyDomain()
 
 class Z3I18NCornerTestCase(TestCaseBase):
 
@@ -59,6 +95,14 @@ class Z3I18NCornerTestCase(TestCaseBase):
         program, macros = self._compile('<span tal:content="msg" />')
         self._check(program,
                     '<span>MSGID</span>\n')
+
+    def test_translate_twodomains(self):
+        program, macros = self._compile(
+                '<div i18n:domain="lower" i18n:translate="">TOLOWER</div>'
+                '<div i18n:domain="upper" i18n:translate="">toupper</div>')
+        self._check(program,
+                    '<div>tolower</div>\n'
+                    '<div>TOUPPER</div>\n')
 
 
 def test_suite():
