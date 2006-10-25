ce002f38a31f5a986c5a6a2b90b88d9eca2f6b1a
Hanno Schlichting <hanno@hannosch.eu>
2006-10-25 21:07:35 +0000

    Back to normal.
    svn path=/PlacelessTranslationService/trunk/; revision=32333

--- Products/PlacelessTranslationService/PlacelessTranslationService.py
+++ Products/PlacelessTranslationService/PlacelessTranslationService.py
@@ -152,7 +152,7 @@ class PlacelessTranslationService(Folder):
     # -3 for alpha, -2 for beta, -1 for release candidate
     # use an internal of > 99 to recreate the PTS at every startup
     # (development mode)
-    _class_version = (1, 4, 0, 0)
+    _class_version = (1, 4, 1, 0)
     all_meta_types = ()
 
     manage_options = Folder.manage_options + (
