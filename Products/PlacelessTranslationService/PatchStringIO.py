821a91e14c9f6690bd00be55ae2514c9f2cdb41c
Hanno Schlichting <hanno@hannosch.eu>
2007-01-20 15:32:38 +0000

    The recent change to return Unicode exposed another place in the TAL interpreter that combines text, which wasn't yet patched to allow a mixture of Unicode and utf-8 encoded text. A new monkey-patch has been introduced to fix this problem. This closes http://dev.plone.org/plone/ticket/6068.
    svn path=/PlacelessTranslationService/branches/1.3/; revision=35957

--- Products/PlacelessTranslationService/PatchStringIO.py
+++ Products/PlacelessTranslationService/PatchStringIO.py
@@ -80,3 +80,52 @@ if hasattr(TALInterpreter, 'StringIO'):
         Z3TALInterpreter.StringIO = patchedStringIO
         pagetemplate.StringIO = patchedStringIO
 
+# PATCH 3
+#
+# Fix TAL engine to tolerate mixed Unicode/encoded strings
+# This patch will not be included in the next PTS version anymore, as these
+# are really a Plone policy decision
+#
+
+import textwrap
+import warnings
+
+def _unicode_replace(structure):
+    if isinstance(structure, str):
+        try:
+            text = structure.decode('ascii')
+        except UnicodeDecodeError:
+            try:
+                text = structure.decode('utf')
+            except UnicodeDecodeError:
+                warnings.warn(textwrap.dedent('''\
+
+                *** *** Insertion of non-unicode non-ascii non-utf8 encoded text in TAL is deprecated !!!
+
+                %s...
+                ''' % (repr(structure), )), DeprecationWarning, 2)
+                # The next line is fool-proof and will substitute ??-s if the
+                # encoding was not unicode
+                text = structure.decode('utf', 'replace')
+    else:
+        text = unicode(structure)
+    return text
+
+def _nulljoin(valuelist):
+    try:
+        return ''.join(valuelist)
+    except UnicodeDecodeError:
+        text = u''
+        for value in valuelist:
+            text += _unicode_replace(value)
+        return text
+
+# Deal with the case where Unicode and encoded strings occur on the same tag.
+from TAL import TALInterpreter
+TALInterpreter._nulljoin_old = TALInterpreter._nulljoin
+TALInterpreter._nulljoin = _nulljoin
+
+if patchZ3:
+    from zope.tal import talinterpreter
+    talinterpreter._nulljoin_old = talinterpreter._nulljoin
+    talinterpreter._nulljoin = _nulljoin
