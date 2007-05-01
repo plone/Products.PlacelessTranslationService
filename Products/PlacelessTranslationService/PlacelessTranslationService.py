40fade1c63cb1507a546d7232f819d9453e5a867
Hanno Schlichting <hanno@hannosch.eu>
2007-05-01 21:19:00 +0000

    Updated class version
    svn path=/PlacelessTranslationService/trunk/; revision=41454

--- Products/PlacelessTranslationService/PlacelessTranslationService.py
+++ Products/PlacelessTranslationService/PlacelessTranslationService.py
@@ -120,7 +120,7 @@ class PlacelessTranslationService(Folder):
     # -3 for alpha, -2 for beta, -1 for release candidate
     # use an internal of > 99 to recreate the PTS at every startup
     # (development mode)
-    _class_version = (1, 4, 2, 0)
+    _class_version = (1, 4, 3, 0)
     all_meta_types = ()
 
     security = ClassSecurityInfo()
