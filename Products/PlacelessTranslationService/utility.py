8f5a44227feb2d2565f092af137a7e50fa13bc7f
Hanno Schlichting <hanno@hannosch.eu>
2007-12-16 23:22:57 +0000

    Raise a ValueError when the Zope3 translation utilities get passed in an invalid context argument. Translations in Zope3 work against the request alone and while the keyword is called context it was too easily confused with a contentish context.
    svn path=/PlacelessTranslationService/branches/1.4/; revision=55669

--- Products/PlacelessTranslationService/utility.py
+++ Products/PlacelessTranslationService/utility.py
@@ -1,5 +1,6 @@
 from zope.interface import implements
 from zope.i18n import interpolate
+from zope.publisher.interfaces.browser import IBrowserRequest
 
 from Products.PlacelessTranslationService import getTranslationService
 from Products.PlacelessTranslationService.interfaces import \
@@ -20,8 +21,13 @@ class PTSTranslationDomain(object):
                   target_language=None, default=None):
 
         pts  = getTranslationService()
-        if pts is None or context is None or 'PARENTS' not in context:
+        if pts is None or context is None:
+            # If we don't have enough context use interpolate
             return interpolate(default, mapping)
+
+        # Don't accept anything which isn't a real request
+        if not IBrowserRequest.providedBy(context) or 'PARENTS' not in context:
+            raise ValueError, "You didn't pass in a request as the context."
 
         parent = context['PARENTS'][-1]
         return pts.translate(self.domain, msgid, mapping, parent,
