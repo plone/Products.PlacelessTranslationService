024a709a89ff63cf13c0ed7c9486b2df9cabc3a0
Hanno Schlichting <hanno@hannosch.eu>
2008-10-16 13:07:15 +0000

    Bring history up to speed
    svn path=/PlacelessTranslationService/trunk/; revision=73757

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
+import os
 from setuptools import setup, find_packages
-import sys, os
 
 version = '2.0a'
 
@@ -7,8 +7,8 @@ setup(name='Products.PlacelessTranslationService',
       version=version,
       description="PTS provides a way of internationalizing (i18n'ing) and "
                   "localizing (l10n'ing) software for Zope 2.",
-      long_description="""\
-      """,
+      long_description=open("readme.txt").read() + "\n" +
+                       open(os.path.join("Products", "PlacelessTranslationService", "HISTORY.txt")).read(),
       classifiers=[
         "Framework :: Plone",
         "Framework :: Zope2",
@@ -18,13 +18,12 @@ setup(name='Products.PlacelessTranslationService',
       keywords='Zope CMF Plone i18n l10n translation gettext',
       author='Lalo Martins',
       author_email='plone-developers@lists.sourceforge.net',
-      url='https://svn.plone.org/svn/collective/PlacelessTranslationService/trunk',
+      url='http://pypi.python.org/pypi/Products.PlacelessTranslationService',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
       zip_safe=False,
-      download_url='http://plone.org/products/pts/releases',
       install_requires=[
         'setuptools',
         'python-gettext >= 0.6',
