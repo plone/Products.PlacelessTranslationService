8d7ab686034896f4c45672e663b24fcc5f642040
Hanno Schlichting <hanno@hannosch.eu>
2010-01-24 17:15:08 +0000

    Extend our hack of the negotiator a bit more and introduce the concept of a template language. This one will always be allowed no matter if there's a po file for it for every domain or not. This closes http://dev.plone.org/plone/ticket/9762.
    svn path=/PlacelessTranslationService/trunk/; revision=109424

--- Products/PlacelessTranslationService/patches.py
+++ Products/PlacelessTranslationService/patches.py
@@ -1,7 +1,35 @@
 # Patch the Zope3 negotiator to cache the negotiated languages
 from Products.PlacelessTranslationService.memoize import memoize_second
+from zope.i18n.interfaces import IUserPreferredLanguages
 from zope.i18n.negotiator import Negotiator
-Negotiator.getLanguage = memoize_second(Negotiator.getLanguage)
+from zope.i18n.negotiator import normalize_langs
+
+TEMPLATE_LANGUAGE = ['en']
+
+def getLanguage(self, langs, env):
+    envadapter = IUserPreferredLanguages(env)
+    userlangs = envadapter.getPreferredLanguages()
+    # Always add the template language to the available ones. This allows the
+    # template language to be picked without the need for a message catalog
+    # for every domain for it to be registered.
+    langs += TEMPLATE_LANGUAGE
+    # Prioritize on the user preferred languages.  Return the
+    # first user preferred language that the object has available.
+    langs = normalize_langs(langs)
+    for lang in userlangs:
+        if lang in langs:
+            return langs.get(lang)
+        # If the user asked for a specific variation, but we don't
+        # have it available we may serve the most generic one,
+        # according to the spec (eg: user asks for ('en-us',
+        # 'de'), but we don't have 'en-us', then 'en' is preferred
+        # to 'de').
+        parts = lang.split('-')
+        if len(parts) > 1 and parts[0] in langs:
+            return langs.get(parts[0])
+    return None
+
+Negotiator.getLanguage = memoize_second(getLanguage)
 
 
 # Patch Zope3 to use a lazy message catalog, but only if we haven't
